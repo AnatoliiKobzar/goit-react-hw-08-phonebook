5a084a506c240c0183aed29c949644ebdb33b2cc
KobzarAnatolii <TohaPirat@gmail.com>
2023-04-13 15:59:40 +0300

    fix validation

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -6,18 +6,8 @@ import {
   Form,
   FormField,
 } from '../ContactForm/ContactForm.styled';
-import * as Yup from 'yup';
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/operations';
-
-const FornSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email address').required('Required'),
-  password: Yup.string()
-    .min(6, 'Password must be 8 characters long')
-    .matches(/[0-9]/, 'Password requires a number')
-    .matches(/[a-z]/, 'Password requires a lowercase letter')
-    .required('Required'),
-});
 
 export const LoginForm = () => {
   const dispath = useDispatch();
@@ -32,7 +22,6 @@ export const LoginForm = () => {
         dispath(logIn(values));
         actions.resetForm();
       }}
-      validationSchema={FornSchema}
     >
       <Form>
         <FormField>
